2b466b5501d66419760274e83db75202cdb03d6a
Christopher Ariagno <christopherariagno@gmail.com>
2020-05-28 12:01:21 -0500

    CSS Fix for link styling. Update build file in blog. Add href to index for blog

--- portfolio/src/main/blog/build-blog.sh
+++ portfolio/src/main/blog/build-blog.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo " --- BUILDING JEKYLL BLOG ---"
 sudo docker run --rm -it --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=production jekyll/jekyll:3.8 jekyll build
-
+echo "--- FINISHED BUILDING, CLEANING BUILD LIBRARIES ---"
+sudo rm -rf vendor/
+rm -rf ../webapp/blog
 mv _site ../webapp/blog
