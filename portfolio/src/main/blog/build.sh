199bdf37ef9553e6ec44b1f2535914adf5df65b3
Christopher Ariagno <christopherariagno@gmail.com>
2020-05-28 11:01:00 -0500

    Clean up docker files, build script made

---
+++ portfolio/src/main/blog/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo docker run --rm -it --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=production jekyll/jekyll:3.8 jekyll build
+
+mv _site ../webapp/blog
